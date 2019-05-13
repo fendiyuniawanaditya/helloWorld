cd080b6b1fd3a9d5302e819d70915f2d34adb19f
fendiya <f3ndi.354@gmail.com>
2019-05-13 13:47:38 +0700

    change helloworld

--- src/main/java/test/poc/nisp/HelloWorld.java
+++ src/main/java/test/poc/nisp/HelloWorld.java
@@ -3,11 +3,48 @@ package test.poc.nisp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloWorld {
 
+    String env = System.getenv("env");
+
     @RequestMapping("/")
     public String index(){
-        return "Hello World";
+        return "Hello World This is From Env "+ env +" IP "+ getCurrentIP();
+    }
+    @RequestMapping("/v1")
+    public String indexv1(){
+        return "Hello World This is From Env "+ env +" IP "+ getIPAddress();
+    }
+
+
+    public String getCurrentIP()  {
+        InetAddress inetAddress = null;
+        try {
+            inetAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String IPAddress = inetAddress.getHostAddress().toString();
+        System.out.println("IP Address:- " + IPAddress);
+        return IPAddress;
+    }
+
+    public String getIPAddress(){
+        String ip = new String();
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ip = socket.getLocalAddress().getHostAddress();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip;
     }
 }
